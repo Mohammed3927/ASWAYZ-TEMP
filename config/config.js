fbcbf7b20cabc4383d48ca57922debd50786fb75
Mohammed3927 <gkhal839@gmail.com>
2025-03-17 03:34:32 +0300

    Update config.js

--- config/config.js
+++ config/config.js
@@ -1,5 +1,5 @@
 module.exports = {
-  discordToken: 'TOKEN', // توكن البوت
+  discordToken: '', // توكن البوت
   joinVoiceChannelId: '1350897895188074637 ', // ايدي الروم لصنع رومك
   categoryId: '1350897837944344616', // ايدي الكاتوجري
   language: 'ar', // ar = للعربي | en = for english
